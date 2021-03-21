e658956ee8d1908a8759fd36550790579802d2fc
shecho <shechovala@gmail.com>
2021-03-21 03:42:15 -0500

    refactor and split code into funcitons

--- index.js
+++ index.js
@@ -11,51 +11,22 @@ const fs = require("fs");
     });
     const table = $(".sortable").find("tr").next();
 
-    // another way to get all data without traversing the elements
-    // console.log(table.text());
-    // console.log(table.html());
-    // let enterprises = [];
-    // table.each((i, e) => {
-    //   // console.log(i, $(e).text());
-    //   enterprises.push($(e).text().split("\n"));
-    // });
-    // console.table(enterprises[2][2]);
-
     let items = [];
     table.each(async (i, el) => {
       const name = $(el).find("a");
       const stockName = $(el).find("small a").next();
       const country = $(el).find("span").next();
-
       const link = $(el).find("a");
-
-      // console.log(link.attr("href"));
-      let uri = link.attr("href");
-      let fullUri = `https://en.wikipedia.org${uri}`;
-      let year = await getYear(fullUri);
-      console.log(year);
-      let item = {
-        name: name.html(),
-        stockName: stockName.html(),
-        country: country.html(),
-        link: link.attr("href"),
-        year: year,
-      };
+      // let uri = link.attr("href");
+      // let fullUri = `https://en.wikipedia.org${uri}`;
+      // let year = await getYear(fullUri);
+      // console.log(year);
+      let item = setItem(name, stockName, country, link);
       items.push(item);
     });
 
-    // console.log(items);
-    // get year by company
+    console.log("line 48", items);
 
-    // const $$ = await request({
-    //   uri: "https://en.wikipedia.org/wiki/ExxonMobil",
-    //   transform: (body) => cheerio.load(body),
-    // });
-
-    // const year = $$(".infobox").find(".noprint");
-    // console.log(year.text());
-
-    // fs.writeFileSync("./data/year.html", year.html());
     fs.writeFileSync("./data/companies.html", table.html());
     fs.writeFileSync("./data/companies.text", table.text());
     // let data = JSON.stringify(companies);
@@ -72,4 +43,14 @@ const getYear = async (uri) => {
   });
   let year = $$(".infobox").find(".noprint").text();
   return year;
+};
+
+const setItem = (name, stockName, country, link) => {
+  let item = {
+    name: name.html(),
+    stockName: stockName.html(),
+    country: country.html(),
+    link: link.attr("href"),
+  };
+  return item;
 };
