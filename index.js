c218c3af23c6a0b0b156ff230c39a472a8f63010
shecho <shechovala@gmail.com>
2021-03-21 01:47:28 -0500

    Initial commit with cherrio config and scraping

---
+++ index.js
@@ -0,0 +1,44 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const fs = require("fs");
+async function init() {
+  try {
+    const $ = await request({
+      uri: "https://en.wikipedia.org/wiki/List_of_largest_energy_companies",
+      transform: (body) => cheerio.load(body),
+    });
+    console.log("new response");
+    const table = $(".sortable").find("tr").next();
+
+    // another way to get all data
+    // console.log(table.text());
+    // console.log(table.html());
+    // let enterprises = [];
+    // table.each((i, e) => {
+    //   // console.log(i, $(e).text());
+    //   enterprises.push($(e).text().split("\n"));
+    // });
+    // console.table(enterprises[2][2]);
+
+    let items = [];
+    table.each((i, el) => {
+      const name = $(el).find("a");
+      const stockName = $(el).find("small a").next();
+      const country = $(el).find("span").next();
+      let item = {
+        name: name.html(),
+        stockName: stockName.html(),
+        country: country.html(),
+      };
+      items.push(item);
+    });
+    console.log(items);
+    fs.writeFileSync("./data/companies.html", table.html());
+    fs.writeFileSync("./data/companies.text", table.text());
+    // let data = JSON.stringify(companies);
+    // fs.writeFileSync("./data/companies.json", data);
+  } catch (e) {
+    console.log(e);
+  }
+}
+init();
---
+++ prueba Jaco.md
@@ -0,0 +1,25 @@
+# Prueba de programación Jaramillo Villegas y Cia Node.js-React.js
+
+<br>
+
+# Primera tarea
+
+1. Cargue y "parsee" los datos del siguiente link: [wikipedia](https://en.wikipedia.org/wiki/List_of_largest_energy_companies)
+
+2. Tome la siguiente información de las primeras 20 compañias
+
+   - Nombre de la compañia
+   - Nombre de la acción
+   - País de origen.
+   - Link (wikipedia) de la compañía.
+
+3. Siga el link de la compañía, y extraiga el año de fundación de la misma. Añada este valor a la información del paso anterior.
+
+4. Ordene el array de compañías basado en el año de fundación (puede ser ascendente o descendente).
+
+5. Almacene la información en un archivo JSON local.
+
+## Para esta tarea puede utilizar librerías de web-scrapping como [cheerio](https://github.com/cheeriojs/cheerio).
+
+
+
